252495812d764280e9a69d1f0c6e8b61112e755f
hwani-gayoung <baek020995@gmail.com>
2026-09-08 19:19:56 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -66,13 +66,13 @@ const CONFIG = {
   // ── 마음 전하실 곳 ──
   accounts: {
     groom: [
-      { role: "신랑", name: "박환이", bank: "국민은행", number: "000-000-000000" },
+      { role: "신랑", name: "박환이", bank: "NH농협은행", number: "302-1209-0836-61" },
       { role: "아버지", name: "박인수", bank: "신한은행", number: "000-000-000000" },
       { role: "어머니", name: "김인숙", bank: "우리은행", number: "000-000-000000" }
     ],
     bride: [
-      { role: "신부", name: "백가영", bank: "하나은행", number: "000-000-000000" },
-      { role: "아버지 · 어머니", name: "김흥자", bank: "기업은행", number: "000-000-000000" },
+      { role: "신부", name: "백가영", bank: "케이뱅크", number: "100-111-743523" },
+      { role: "아버지 · 어머니", name: "백상열", bank: "우리은행", number: "087-056686-02-002" },
     ]
   },
 
